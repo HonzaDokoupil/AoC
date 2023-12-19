252394e4ea420b78b6c295e1bb16354340e180e1
honzaDokoupil <dokoupil.honza1@seznam.cz>
2023-12-19 16:18:11 +0100

    Add project files.

---
+++ 4.12.2023/Program.cs
@@ -0,0 +1,60 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+int winningPoints = 0;
+foreach (string line in input)
+{
+    string[] card = line.Split(':');
+    string numbers = card[1];
+    string[] numbersToArray = numbers.Split('|');
+    string numbersLeft = numbersToArray[0];
+    string numbersRight = numbersToArray[1];
+    string[] numbersLA = numbersLeft.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    string[] numbersRA = numbersRight.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int counter = 0;
+    int cardPoints = 0;
+    foreach (string number in numbersLA)
+    {
+        
+        if (numbersRA.Contains(number))
+        {
+            if (counter == 0)
+            {
+                cardPoints += 1;
+                counter++;
+            }
+            else
+            {
+                cardPoints *= 2;
+            }                        
+        }       
+    }
+    winningPoints += cardPoints;
+}
+Console.WriteLine(winningPoints);
+// second part
+
+int[] scratchcards = new int[input.Length];
+for (int i = 0; i < scratchcards.Length; i++)
+{
+    scratchcards[i] = 1;
+}
+for (int i = 0; i < input.Length; i++)
+{
+	string[] card = input[i].Split(':');
+	string numbers = card[1];
+	string[] numbersToArray = numbers.Split('|');
+	string numbersLeft = numbersToArray[0];
+	string numbersRight = numbersToArray[1];
+	string[] numbersLA = numbersLeft.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+	string[] numbersRA = numbersRight.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int counter = 1;
+    for (int j = 0; j < numbersLA.Length; j++)
+    {
+        if (numbersRA.Contains(numbersLA[j]))
+        {
+            scratchcards[i + counter] = scratchcards[i + counter] + 1 * scratchcards[i];
+            counter++;
+        }
+    }
+}
+int scratchcardsTotal = scratchcards.Sum();
+Console.WriteLine(scratchcardsTotal);
---
+++ 6.12.2023/Program.cs
@@ -0,0 +1,31 @@
+﻿var input = File.ReadAllLines("input.txt");
+var raceTimes = input[0].Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+var raceDistances = input[1].Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+int numberOfWays = 1;
+for (int i = 1; i < raceTimes.Length; i++)
+{
+	int actualWays = 0;
+	int time = int.Parse(raceTimes[i]);
+	int distance = int.Parse(raceDistances[i]);
+	for (int j = 0; j < time; j++)
+	{
+		if (j * (time - j) > distance)
+		{
+			actualWays++;
+		}
+	}
+	numberOfWays *= actualWays;
+}
+Console.WriteLine(numberOfWays);
+// part 2
+long raceTime = long.Parse(string.Join("", raceTimes.Skip(1)));
+long raceDistance = long.Parse(string.Join("", raceDistances.Skip(1)));
+long ways = 0;
+for (int i = 0;i < raceTime; i++)
+{
+	if (i * (raceTime - i) > raceDistance)
+	{
+		ways++;
+	}
+}
+Console.WriteLine(ways);
