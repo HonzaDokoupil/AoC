5876ed9da0006a02c1291dda210825ba1bfd9ccb
honzaDokoupil <dokoupil.honza1@seznam.cz>
2024-01-04 10:25:59 +0100

    commented

--- Day_10/Program.cs
+++ Day_10/Program.cs
@@ -9,13 +9,13 @@ internal class Program
         string[] input = File.ReadAllLines("Input.txt");
         //part 1
         string pipes = "-|FJ7L";
-        char startingPoint = 'S';
+        const char startingPoint = 'S';
         int rows = 0;
         int cols = 0;
         int result = 0;
-        var lists = new List<List<char>>();     // for part 2
+        var lists = new List<List<char>>();                                             // lists for part 2
         int result2 = 0;                        
-        for (int i = 0; i < input.Length; i++)  
+        for (int i = 0; i < input.Length; i++)                                          // fill lists with space for part 2
         {                                       
             lists.Add(new List<char>());        
             for (int j = 0 ; j < input[i].Length ; j++)
@@ -23,11 +23,11 @@ internal class Program
                 lists[i].Add(' ');              
             }                                   
         }                                     
-        for (int i = 0; i < input.Length; i++)
+        for (int i = 0; i < input.Length; i++)                                          // find start
         {
             for (int j = 0; j < input[i].Length; j++)
             {
-                if (input[i][j] == 'S')
+                if (input[i][j] == startingPoint)
                 {
                     rows = i;
                     cols = j;
@@ -39,44 +39,44 @@ internal class Program
         string pastLocation = "";
         do
         {   
-            lists[rows][cols] = currentLocation; // for part 2
-            if (CurrentCheck(currentLocation, pipes, startingPoint, 0, 3, 4) && 
-                NextCheck(input, rows, cols - 1, pipes, 0, 2, 5, startingPoint) &&
-                PastCheck(pastLocation, "left", ""))
+            lists[rows][cols] = currentLocation;                                        // print pipes in lists for part 2
+            if (CurrentCheck(currentLocation, pipes, startingPoint, 0, 3, 4) &&         // if current is - or J or 7
+                NextCheck(input, rows, cols - 1, pipes, 0, 2, 5, startingPoint) &&      // if next is - or F or L
+                PastCheck(pastLocation, "left", ""))                                    // if past is start or not left  
             {
-                currentLocation = input[rows][cols - 1]; // go left
+                currentLocation = input[rows][cols - 1];                                // go left
                 cols = cols - 1;
                 result++;
                 pastLocation = "right";
             }
-            else if (CurrentCheck(currentLocation, pipes, startingPoint, 0, 2, 5) &&
-                NextCheck(input, rows, cols + 1, pipes, 0, 3, 4, startingPoint) &&
-                PastCheck(pastLocation, "right", ""))
+            else if (CurrentCheck(currentLocation, pipes, startingPoint, 0, 2, 5) &&    // if current is - or F or L
+                NextCheck(input, rows, cols + 1, pipes, 0, 3, 4, startingPoint) &&      // if next is - or J or 7
+                PastCheck(pastLocation, "right", ""))                                   // if past is start or not right   
             {
-                currentLocation = input[rows][cols + 1]; // go right
+                currentLocation = input[rows][cols + 1];                                // go right
                 cols = cols + 1;
                 result++;
                 pastLocation = "left";
             }
-            else if (CurrentCheck(currentLocation, pipes, startingPoint, 1, 3, 5) &&
-                NextCheck(input, rows - 1, cols, pipes, 1, 2, 4, startingPoint) &&
-                PastCheck(pastLocation, "up", ""))
+            else if (CurrentCheck(currentLocation, pipes, startingPoint, 1, 3, 5) &&    // if current is | or J or L 
+                NextCheck(input, rows - 1, cols, pipes, 1, 2, 4, startingPoint) &&      // if next is | or F or 7
+                PastCheck(pastLocation, "up", ""))                                      // if past is start or not up 
             {
-                currentLocation = input[rows - 1][cols]; // go up
+                currentLocation = input[rows - 1][cols];                                // go up
                 rows = rows - 1;
                 result++;
                 pastLocation = "down";
             }
-            else if (CurrentCheck(currentLocation, pipes, startingPoint, 1, 2, 4) &&
-                NextCheck(input, rows + 1, cols, pipes, 1, 3, 5, startingPoint) &&
-                PastCheck(pastLocation, "down", ""))
+            else if (CurrentCheck(currentLocation, pipes, startingPoint, 1, 2, 4) &&    // if current is | or F or 7
+                NextCheck(input, rows + 1, cols, pipes, 1, 3, 5, startingPoint) &&      // if next is | or J or L
+                PastCheck(pastLocation, "down", ""))                                    // if past is start or not down
             {
-                currentLocation = input[rows + 1][cols]; //go down
+                currentLocation = input[rows + 1][cols];                                //go down
                 rows = rows + 1;
                 result++;
                 pastLocation = "up";
             }
-        } while (currentLocation != 'S');
+        } while (currentLocation != startingPoint);                                               
         Console.WriteLine(result / 2);
         // part 2
         for (int i = 0; i < lists.Count; i++)
@@ -87,23 +87,24 @@ internal class Program
                 if (space == ' ')
                 {
                     int count = 0;
-                    for (int k = i, l = j; k >= 0 && l >= 0; k--, l--)
+                    for (int k = i, l = j; k >= 0 && l >= 0; k--, l--)                  // search diagonaly ( \ ) for crossings
                     {
                         char next = lists[k][l];
-                        if (next == pipes[0] || next == pipes[1] || next == pipes[2] || next == pipes[3] || next == startingPoint)
+                        if (next == pipes[0] || next == pipes[1] || next == pipes[2] || 
+                            next == pipes[3] || next == startingPoint)                  // 7 and L are not crossed
                         {
-                            count++;
+                            count++;                                                    // and count crossing the pipes
                         }
                     }
-                    if (count % 2 == 0)
+                    if (count % 2 == 0)                                                 // means we are outside
                     {
-                       lists[i][j] = '0';
+                       lists[i][j] = '0';                                               // mark outside as 0
                     }
                 }
 
             }
         }
-        foreach (var list in lists)
+        foreach (var list in lists)                                                     // count remaining titles as inside
         {
             foreach (char c in list)
             {
